9145144c13b1df4dfe8910886c485e13ac8cee22
Marc LANTOL <marc.lantol@gmail.com>
2023-07-28 12:01:02 +0200

    Style: Observatoire section

--- frontend/src/components/Observatoire/Observatoire.jsx
+++ frontend/src/components/Observatoire/Observatoire.jsx
@@ -4,8 +4,9 @@ import "./Observatoire.css";
 
 function Observatoire() {
   return (
-    <section className="container-observatoire">
-      <p className="font-exo2 text-white opacity-60 text-justify text-sm pt-2">
+    <section className="section-observatoire">
+      <h1 className="observatoire-title">L'observatoire</h1>
+      <p className="observatoire-paragraph">
         Depuis 1985, l'Association accueille le public dans le but de promouvoir
         l'astronomie amateur. Situés en plein coeur de la ville de Saint Jean Le
         Blanc, nous vous proposons de découvrir le ciel au travers de nos
