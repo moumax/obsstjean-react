f9b5133b829a5e282d68c3dc088c39f7a9a51e96
Marc LANTOL <marc.lantol@gmail.com>
2023-08-11 16:35:38 +0200

    Add some logos on footer Fixes #4 Close #4

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,6 +1,8 @@
 import React, { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
+import { BiLogoJavascript, BiLogoReact } from "react-icons/bi";
+import { SiMysql } from "react-icons/si";
 import CurrentUserContext from "../../contexts/userContext";
 import axiosAPI from "../../services/axiosAPI";
 
@@ -26,6 +28,16 @@ function Footer() {
       <div className="footer-informations">
         <p>© 2019 - 2023 -- Marc LANTOL</p>
         <p>Observatoire Astronomique de Saint Jean Le Blanc (V3.0.0)</p>
+        <div className="footer-logos">
+          <BiLogoReact size="2rem" color="#61DAFB" />
+          <BiLogoJavascript size="2rem" color="yellow" />
+          <SiMysql size="2rem" color="#3E6E93" />
+          <img
+            className="footer-img"
+            src="src/assets/footer/stjeanleblanc.png"
+            alt="Ville de Saint Jean Le Blanc"
+          />
+        </div>
       </div>
       <div className="footer-buttons">
         {!user && (
